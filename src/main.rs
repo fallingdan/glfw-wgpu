0a55eaafb096916ee0423e3bfda6d6d2133efd93
Dan <onewaylimited@gmail.com>
2026-05-23 15:20:05 -0500

    basic window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use glfw
+use glfw::{self, Context, fail_on_errors};
 
 fn main() {
     println!("Hello, world!");
 
-    let glfw = glfw::Win
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw
+        .create_window(800, 600, "Beginner WGPU", glfw::WindowMode::Windowed)
+        .unwrap();
+
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+        window.swap_buffers();
+    }
 }
