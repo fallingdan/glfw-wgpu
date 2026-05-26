803b137d49215905170929b0aa251f489814be20
falling_dan <onewaylimited@gmail.com>
2026-05-26 12:29:31 -0500

    Begin building out wgpu surface

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ struct State<'a> {
     device: wgpu::Device,       // The handle for the GPU
     queue: wgpu::Queue,         // Where we will submit work to get done
     config: wgpu::SurfaceConfiguration,
-    size: (i32, i32),
+    width: i32,
+    height: i32,
     window: &'a mut Window,
 }
 
-impl<'a> State {
+impl<'a> State<'a> {
     async fn new(window: &'a mut Window) -> Self {
         let size = window.get_size();
+        let width = size.0;
+        let height = size.1;
 
         let instance_descriptor = wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
@@ -34,6 +37,49 @@ impl<'a> State {
         };
 
         let adapter = instance.request_adapter(&adapter_descriptor).await.unwrap();
+
+        let device_descriptor = wgpu::DeviceDescriptor {
+            label: Some("Device"),
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::defaults(),
+            experimental_features: wgpu::ExperimentalFeatures::disabled(),
+            memory_hints: wgpu::MemoryHints::default(),
+            trace: wgpu::Trace::Off,
+        };
+
+        let (device, queue) = adapter.request_device(&device_descriptor).await.unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(surface_capabilities.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: width as u32,
+            height: height as u32,
+            present_mode: surface_capabilities.present_modes[0],
+            desired_maximum_frame_latency: 2,
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &config);
+
+        Self {
+            instance,
+            surface,
+            device,
+            queue,
+            config,
+            width,
+            height,
+            window,
+        }
     }
 }
 
